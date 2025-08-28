27df9dd3e8cfe23c661b1bc641ece0e492360895
Imantas Precas <precasimantas@gmail.com>
2025-08-28 09:09:27 +0300

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/sports-standings-app/",
+  base: "/sports-standings-app",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
