6cae004c6574b93a27ea1210c6cd303dc33955ed
Imantas Precas <precasimantas@gmail.com>
2025-08-28 15:35:07 +0300

    enable persistence for Premier League slice

--- src/store/appStore.tsx
+++ src/store/appStore.tsx
@@ -1,13 +1,16 @@
 import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
 import { createPremierLeagueSlice, type IPremierLeagueSlice } from './premierLeagueSlice'
+import { devtools } from 'zustand/middleware'
 
 export type StoreType = IPremierLeagueSlice
 
 const useAppStore = create<IPremierLeagueSlice>()(
-  immer((...a) => ({
-    ...createPremierLeagueSlice(...a),
-  }))
+  immer(
+    devtools((...a) => ({
+      ...createPremierLeagueSlice(...a),
+    }))
+  )
 )
 
 export default useAppStore
--- src/store/premierLeagueSlice.tsx
+++ src/store/premierLeagueSlice.tsx
@@ -1,6 +1,6 @@
 import type { StateCreator } from 'zustand'
 import type { StoreType } from './appStore'
-// import { persist } from 'zustand/middleware'
+import { persist } from 'zustand/middleware'
 import type { JSX } from 'react'
 
 interface Team {
@@ -49,122 +49,103 @@ export interface IMatch {
 export const createPremierLeagueSlice: StateCreator<
   StoreType,
   [['zustand/immer', never]],
-  [],
-  // [['zustand/persist', IPremierLeagueSlice]],
+  // [],
+  [['zustand/persist', IPremierLeagueSlice]],
   IPremierLeagueSlice
-> = (set, get) => ({
-  // Initial teams and standings for development
-  teams: {
-    arsenal: { id: 'arsenal', name: 'Arsenal' },
-    chelsea: { id: 'chelsea', name: 'Chelsea' },
-  },
-  standings: {
-    arsenal: {
-      teamId: 'arsenal',
-      name: 'Arsenal',
-      matches: 0,
-      won: 0,
-      drawn: 0,
-      lost: 0,
-      points: 0,
+> = persist(
+  (set, get) => ({
+    // Initial teams and standings for development
+    teams: {},
+    standings: {},
+    matches: {},
+    duplicatedMatch: false,
+    addTeam: (name: string) => {
+      const id = name.toLowerCase().replace(/\s+/g, '-')
+      set((state) => {
+        state.teams[id] = { id, name }
+        state.standings[id] = {
+          teamId: id,
+          name,
+          matches: 0,
+          won: 0,
+          drawn: 0,
+          lost: 0,
+          points: 0,
+        }
+      })
     },
-    chelsea: {
-      teamId: 'chelsea',
-      name: 'Chelsea',
-      matches: 0,
-      won: 0,
-      drawn: 0,
-      lost: 0,
-      points: 0,
+    getTeamsList: () => {
+      const { teams } = get()
+      return Object.entries(teams).map(([id, team]) => ({ id, name: team.name }))
     },
-  },
-  matches: {},
-  duplicatedMatch: false,
-  addTeam: (name: string) => {
-    const id = name.toLowerCase().replace(/\s+/g, '-')
-    set((state) => {
-      state.teams[id] = { id, name }
-      state.standings[id] = {
-        teamId: id,
-        name,
-        matches: 0,
-        won: 0,
-        drawn: 0,
-        lost: 0,
-        points: 0,
-      }
-    })
-  },
-  getTeamsList: () => {
-    const { teams } = get()
-    return Object.entries(teams).map(([id, team]) => ({ id, name: team.name }))
-  },
-  getStandingsTable: () => {
-    const standings = Object.values(get().standings).sort((a, b) => b.points - a.points)
-    return {
-      tableHeader: [
-        { title: 'Team', key: 'name' },
-        { title: 'P', key: 'matches' },
-        { title: 'W', key: 'won' },
-        { title: 'D', key: 'drawn' },
-        { title: 'L', key: 'lost' },
-        { title: 'Pts', key: 'points' },
-      ],
-      standings,
-    }
-  },
-  addMatch: (teamA, teamB, scoreA, scoreB) => {
-    const id = crypto.randomUUID()
-    set((state) => {
-      state.matches[id] = { id, teamA, teamB, scoreA, scoreB }
-    })
-
-    let resultA: 'win' | 'loss' | 'draw' = 'draw'
-    let resultB: 'win' | 'loss' | 'draw' = 'draw'
-
-    if (scoreA > scoreB) {
-      resultA = 'win'
-      resultB = 'loss'
-    } else if (scoreA < scoreB) {
-      resultA = 'loss'
-      resultB = 'win'
-    }
-
-    const updateTeamStats = (teamId: string, result: string) => {
-      const teamStats = get().standings[teamId]
-      const matches = teamStats.matches + 1
-      let won = teamStats.won
-      let drawn = teamStats.drawn
-      let lost = teamStats.lost
-      let points = teamStats.points
-
-      if (result === 'win') {
-        won += 1
-        points += 3
-      } else if (result === 'draw') {
-        drawn += 1
-        points += 1
-      } else if (result === 'loss') {
-        lost += 1
-      }
-
+    getStandingsTable: () => {
+      const standings = Object.values(get().standings).sort((a, b) => b.points - a.points)
       return {
-        teamId,
-        matches,
-        won,
-        drawn,
-        lost,
-        points,
-        name: teamStats.name,
+        tableHeader: [
+          { title: 'Team', key: 'name' },
+          { title: 'P', key: 'matches' },
+          { title: 'W', key: 'won' },
+          { title: 'D', key: 'drawn' },
+          { title: 'L', key: 'lost' },
+          { title: 'Pts', key: 'points' },
+        ],
+        standings,
       }
-    }
+    },
+    addMatch: (teamA, teamB, scoreA, scoreB) => {
+      const id = crypto.randomUUID()
+      set((state) => {
+        state.matches[id] = { id, teamA, teamB, scoreA, scoreB }
+      })
 
-    set((state) => {
-      state.standings[teamA] = updateTeamStats(teamA, resultA)
-      state.standings[teamB] = updateTeamStats(teamB, resultB)
-    })
-  },
-  getMatches: () => {
-    return Object.values(get().matches)
-  },
-})
+      let resultA: 'win' | 'loss' | 'draw' = 'draw'
+      let resultB: 'win' | 'loss' | 'draw' = 'draw'
+
+      if (scoreA > scoreB) {
+        resultA = 'win'
+        resultB = 'loss'
+      } else if (scoreA < scoreB) {
+        resultA = 'loss'
+        resultB = 'win'
+      }
+
+      const updateTeamStats = (teamId: string, result: string) => {
+        const teamStats = get().standings[teamId]
+        const matches = teamStats.matches + 1
+        let won = teamStats.won
+        let drawn = teamStats.drawn
+        let lost = teamStats.lost
+        let points = teamStats.points
+
+        if (result === 'win') {
+          won += 1
+          points += 3
+        } else if (result === 'draw') {
+          drawn += 1
+          points += 1
+        } else if (result === 'loss') {
+          lost += 1
+        }
+
+        return {
+          teamId,
+          matches,
+          won,
+          drawn,
+          lost,
+          points,
+          name: teamStats.name,
+        }
+      }
+
+      set((state) => {
+        state.standings[teamA] = updateTeamStats(teamA, resultA)
+        state.standings[teamB] = updateTeamStats(teamB, resultB)
+      })
+    },
+    getMatches: () => {
+      return Object.values(get().matches)
+    },
+  }),
+  { name: 'premierLeagueSlice' }
+)
